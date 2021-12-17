fcf37682a1032782f2fae00b3f6b8c24ee697691
dollking <dollking@kookmin.ac.kr>
2021-12-17 13:05:48 +0900

    add caltech101

--- data/transform.py
+++ data/transform.py
@@ -7,10 +7,27 @@ class Cifar(object):
             T.RandomHorizontalFlip(),
             T.RandomCrop(size=32, padding=4),
             T.ToTensor(),
-            T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
         ])
 
         self.test_transform = T.Compose([
             T.ToTensor(),
-            T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        ])
+
+
+class Caltech(object):
+    def __init__(self):
+        self.train_transform = T.Compose([
+            T.Grayscale(),
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.RandomHorizontalFlip(),
+            T.RandomCrop(size=224, padding=4),
+            T.ToTensor(),
+        ])
+
+        self.test_transform = T.Compose([
+            T.Grayscale(),
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.ToTensor(),
         ])
