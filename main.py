695d234427f9e72aa66c14f297299af2a2468eb3
dollking <dollking@kookmin.ac.kr>
2021-12-28 20:37:03 +0900

    init

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.datasets import CIFAR100, CIFAR10
 
 from tqdm import tqdm
+from tensorboardX import SummaryWriter
 
 from config import *
 from models.resnet import ResNet18
-from models.lossnet import LossNet
+from models.vq_vae import VQVAE
 from data.transform import Cifar
 from data.sampler import SubsetSequentialSampler
 
@@ -40,56 +41,49 @@ else:
     raise FileExistsError
 
 
-def loss_pred_loss(input, target, margin=1.0, reduction='mean'):
-    assert len(input) % 2 == 0, 'the batch size is not even.'
-    assert input.shape == input.flip(0).shape
-    
-    input = (input - input.flip(0))[:len(input)//2]
-    target = (target - target.flip(0))[:len(target)//2]
-    target = target.detach()
-
-    one = 2 * torch.sign(torch.clamp(target, min=0)) - 1
-
-    if reduction == 'mean':
-        loss = torch.sum(torch.clamp(margin - one * input, min=0))
-        loss = loss / input.size(0)
-    elif reduction == 'none':
-        loss = torch.clamp(margin - one * input, min=0)
-    else:
-        NotImplementedError()
-    
-    return loss
-
-
-def train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss):
+def train_backbone_epoch(models, criterions, optimizers, dataloaders):
     models['backbone'].train()
-    models['module'].train()
+    models['uncertainty'].eval()
 
     for data in tqdm(dataloaders['train'], leave=False, total=len(dataloaders['train'])):
         inputs = data[0].cuda()
         labels = data[1].cuda()
 
         optimizers['backbone'].zero_grad()
-        optimizers['module'].zero_grad()
 
         scores, features = models['backbone'](inputs)
-        target_loss = criterion(scores, labels)
-
-        if epoch > epoch_loss:
-            features[0] = features[0].detach()
-            features[1] = features[1].detach()
-            features[2] = features[2].detach()
-            features[3] = features[3].detach()
-        pred_loss = models['module'](features)
-        pred_loss = pred_loss.view(pred_loss.size(0))
-
-        m_backbone_loss = torch.sum(target_loss) / target_loss.size(0)
-        m_module_loss = loss_pred_loss(pred_loss, target_loss, margin=MARGIN)
-        loss = m_backbone_loss + WEIGHT * m_module_loss
+        loss = criterions['backbone'](scores, labels)
 
         loss.backward()
         optimizers['backbone'].step()
-        optimizers['module'].step()
+
+
+def train_uncertainty_epoch(models, criterions, optimizers, dataloaders, summary_writer, epoch):
+    models['backbone'].eval()
+    models['uncertainty'].train()
+
+    cnt = 0
+    _loss = 0.
+    for data in tqdm(dataloaders['train'], leave=False, total=len(dataloaders['train'])):
+        inputs = data[0].cuda()
+
+        optimizers['uncertainty'].zero_grad()
+
+        vq_loss, recon, _, _ = models['backbone'](inputs)
+        recon_loss = criterions['uncertainty'](recon, inputs)
+
+        loss = vq_loss + (torch.sum(recon_loss) / recon_loss.size(0))
+
+        loss.backward()
+        optimizers['uncertainty'].step()
+
+        _loss += loss
+
+        summary_writer.add_image('image/origin', inputs[0], epoch)
+        summary_writer.add_image('image/recon', recon[0], epoch)
+        summary_writer.add_scalar('loss', _loss / cnt, epoch)
+
+    return _loss / cnt
 
 
 def test(models, dataloaders, mode='val'):
@@ -111,7 +105,9 @@ def test(models, dataloaders, mode='val'):
     return 100 * correct / total
 
 
-def train(models, criterion, optimizers, schedulers, dataloaders, num_epochs, epoch_loss):
+def train(models, criterions, optimizers, schedulers, dataloaders, num_epochs):
+    summary_writer = SummaryWriter(log_dir=os.path.join('./'), comment='VAE')
+
     print('>> Train a Model.')
 
     checkpoint_dir = os.path.join(f'./trained', 'weights')
@@ -120,27 +116,28 @@ def train(models, criterion, optimizers, schedulers, dataloaders, num_epochs, ep
     
     for epoch in range(num_epochs):
         schedulers['backbone'].step()
-        schedulers['module'].step()
+        train_backbone_epoch(models, criterions, optimizers, dataloaders)
 
-        train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss)
+    for epoch in range(num_epochs):
+        loss = train_uncertainty_epoch(models, criterions, optimizers, dataloaders, summary_writer, epoch)
+        schedulers['uncertainty'].step(loss)
 
     print('>> Finished.')
 
 
-def get_uncertainty(models, unlabeled_loader):
+def get_uncertainty(models, criterions, unlabeled_loader):
     models['backbone'].eval()
-    models['module'].eval()
+    models['uncertainty'].eval()
 
     uncertainty = torch.tensor([]).cuda()
     with torch.no_grad():
         for (inputs, labels) in unlabeled_loader:
             inputs = inputs.cuda()
 
-            scores, features = models['backbone'](inputs)
-            pred_loss = models['module'](features)
-            pred_loss = pred_loss.view(pred_loss.size(0))
+            _, recon, _, _ = models['uncertainty'](inputs)
+            loss = criterions['uncertainty'](inputs)
 
-            uncertainty = torch.cat((uncertainty, pred_loss), 0)
+            uncertainty = torch.cat((uncertainty, loss), 0)
     
     return uncertainty.cpu()
 
@@ -160,26 +157,27 @@ if __name__ == '__main__':
         test_loader = DataLoader(data_test, batch_size=BATCH)
         dataloaders = {'train': train_loader, 'test': test_loader}
 
-        loss_module = LossNet().cuda()
+        uncertainty_model = VQVAE(NUM_HIDDENS, NUM_RESIDUAL_LAYERS, NUM_RESIDUAL_HIDDENS, NUM_EMBEDDINGS,
+                                  EMBEDDING_DIM, COMMITMENT_COST, DECAY).cuda()
         resnet18 = ResNet18(num_classes=CLS_CNT).cuda()
-        models = {'backbone': resnet18, 'module': loss_module}
+        models = {'backbone': resnet18, 'uncertainty': uncertainty}
 
         torch.backends.cudnn.benchmark = False
 
         for cycle in range(CYCLES):
-            criterion = nn.CrossEntropyLoss(reduction='none').cuda()
+            criterion_backbone = nn.CrossEntropyLoss().cuda()
+            criterion_uncertainty = nn.MSELoss(reduction='none').cuda()
+            criterions = {'backbone': criterion_backbone, 'uncertainty': criterion_uncertainty}
 
-            optim_backbone = optim.SGD(models['backbone'].parameters(), lr=LR,
-                                       momentum=MOMENTUM, weight_decay=WDECAY)
-            optim_module = optim.SGD(models['module'].parameters(), lr=LR,
-                                     momentum=MOMENTUM, weight_decay=WDECAY)
-            optimizers = {'backbone': optim_backbone, 'module': optim_module}
+            optim_backbone = optim.SGD(models['backbone'].parameters(), lr=LR, momentum=MOMENTUM, weight_decay=WDECAY)
+            optim_uncertainty = optim.Adam(models['uncertainty'].parameters())
+            optimizers = {'backbone': optim_backbone, 'uncertainty': optim_uncertainty}
 
             sched_backbone = lr_scheduler.MultiStepLR(optim_backbone, milestones=MILESTONES)
-            sched_module = lr_scheduler.MultiStepLR(optim_module, milestones=MILESTONES)
-            schedulers = {'backbone': sched_backbone, 'module': sched_module}
+            sched_uncertainty = lr_scheduler.ReduceLROnPlateau(optim_uncertainty, mode='min', factor=0.8, cooldown=4)
+            schedulers = {'backbone': sched_backbone, 'uncertainty': sched_uncertainty}
 
-            train(models, criterion, optimizers, schedulers, dataloaders, EPOCH, EPOCHL)
+            train(models, criterions, optimizers, schedulers, dataloaders, EPOCH)
             acc = test(models, dataloaders, mode='test')
 
             fp.write(f'{acc}\n')
@@ -193,7 +191,7 @@ if __name__ == '__main__':
                                           sampler=SubsetSequentialSampler(subset),
                                           pin_memory=True)
 
-            uncertainty = get_uncertainty(models, unlabeled_loader)
+            uncertainty = get_uncertainty(models, criterions, unlabeled_loader)
 
             arg = np.argsort(uncertainty)
 
